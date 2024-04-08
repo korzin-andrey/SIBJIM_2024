6d2be81c55cb1c0cf1ad3be0097d0a5883f45a11
unknown <corzin.an@gmail.com>
2024-04-08 14:36:55 +0300

    v 0.1

---
+++ data/README.txt
@@ -0,0 +1,3 @@
+This directory is for storing input data for modeling and sampling. 
+Populations for Samara and Chelyabinsk are provided by this link: https://disk.yandex.ru/d/BrcX8QUBlDOEug.
+Put downloaded data into this directory.
--- simulation/agent_based_model.py
+++ simulation/agent_based_model.py
@@ -20,14 +20,42 @@ pd.options.display.width = None
 pd.options.display.max_columns = None
 
 
-# forlorn_v2 - вторичное использование случайных чисел из массива данных
-# multistrain - несколько штаммов
-# hybrid - рефакторинг для гибридной модели
+def load_data(data_path):
+    data = pd.read_csv(data_path + 'people.txt', sep='\t', index_col=0)
+    data = data[['sp_id', 'sp_hh_id', 'age', 'sex', 'work_id']]
+    households = pd.read_csv(data_path + 'households.txt', sep='\t')
+    households = households[['sp_id', 'latitude', 'longitude']]
+    dict_school_id = json.load(
+        open(os.path.expanduser(data_path + 'schools.json')))
+    dict_school_len = [len(dict_school_id[i]) for i in dict_school_id.keys()]
+    return data, households, dict_school_id
 
+
+def preprocess_data(data, households, dict_school_id):
+    data[['sp_id', 'sp_hh_id', 'age']] = data[[
+        'sp_id', 'sp_hh_id', 'age']].astype(int)
+    data[['work_id']] = data[['work_id']].astype(str)
+    data = data.sample(frac=1)
+    households[['sp_id']] = households[['sp_id']].astype(int)
+    households[['latitude', 'longitude']] = households[[
+        'latitude', 'longitude']].astype(float)
+    households.index = households.sp_id
+    return data, households, dict_school_id
+
+
+# infected_init_dic = {'H1N1': 10, 'H3N2': 0, 'B': 0}
+# alpha_dic = {'H1N1': 0.78, 'H3N2': 0.74, 'B': 0.6}
+
+# POP_MODEL_MOMENT = 1000  # Момент переключения
+# # Длина запоминаемой предыстории для популяционной модели
+# MAGIC_PREHISTORY_LENGTH = 10
+
+# lmbd = 0.15
+# num_runs = 10
+# days = range(1, 150)
+# PERCENT_PROTECTED = 1  # Эмуляция разреженной сети, согласно RJNAMM
 
 strains_keys = ['H1N1', 'H3N2', 'B']
-infected_init_dic = {'H1N1': 10, 'H3N2': 0, 'B': 0}
-POP_MODEL_MOMENT = 500
 
 
 def strainForInfIndex(idx):  # idx from 1
@@ -55,17 +83,6 @@ def func_b_r(inf_day):
     else:
         return 0
 
-
-# Условие переключения на популяционную модель
-def switchToPopModelClause(cur_step, mode="One_traj"):
-    if mode == "One_traj":
-        return (cur_step == POP_MODEL_MOMENT)
-
-    if mode == "One_traj_sequential":
-        return (cur_step > POP_MODEL_MOMENT)
-
-    return (cur_step > POP_MODEL_MOMENT)
-
 # def calcPhiForPop(infected_list): #Подсчитать зараженными разными штаммами
 #
 #     Phi_arr = np.zeros((len(strains_keys), MAGIC_PREHISTORY_LENGTH))
@@ -80,21 +97,21 @@ def switchToPopModelClause(cur_step, mode="One_traj"):
 #     return Phi_arr
 
 
-def calcPhiForPop(infected_list):  # Подсчитать зараженными разными штаммами
+# def calcPhiForPop(infected_list):  # Подсчитать зараженными разными штаммами
 
-    len_data = min(MAGIC_PREHISTORY_LENGTH, len(infected_list['H1N1']))
-    Phi_arr = np.zeros((len(strains_keys), len_data))
-    # print("Total people")
-    # print(people_df.shape[0])
+#     len_data = min(MAGIC_PREHISTORY_LENGTH, len(infected_list['H1N1']))
+#     Phi_arr = np.zeros((len(strains_keys), len_data))
+#     # print("Total people")
+#     # print(people_df.shape[0])
 
-    for idx, key in enumerate(strains_keys):  # Считаем инфекционных
-        infected_list[key].reverse()
-        number_infected_by_strain = infected_list[key]
-        number_infected_by_strain_arr = np.array(
-            number_infected_by_strain)[:len_data]
-        Phi_arr[idx] = number_infected_by_strain_arr.copy()
+#     for idx, key in enumerate(strains_keys):  # Считаем инфекционных
+#         infected_list[key].reverse()
+#         number_infected_by_strain = infected_list[key]
+#         number_infected_by_strain_arr = np.array(
+#             number_infected_by_strain)[:len_data]
+#         Phi_arr[idx] = number_infected_by_strain_arr.copy()
 
-    return Phi_arr
+#     return Phi_arr
 
 
 # Подсчитать полностью иммунных (переболевших в этом сезоне)
@@ -123,7 +140,7 @@ def calcImmuneFractionForPop(people_df):
     return ImmuneFraction_list
 
 
-def main_function(number_seed, dataset, dict_school_id_all, lmbd, days, dict_school_len, res_dir):
+def main_function(number_seed, data_folder, dataset, dict_school_id_all, lmbd, infected_init_dic, days, dict_school_len):
     np.random.seed(number_seed)
     data_current = dataset.copy()
     y = {}
@@ -450,26 +467,6 @@ def main_function(number_seed, dataset, dict_school_id_all, lmbd, days, dict_sch
                 'sp_id', 'sp_hh_id']]
             dataset_infected_by_strain = dataset_infected_by_strain.sort_values(by=[
                                                                                 'sp_hh_id'])
-
-            # Добавляем координаты к домохозяйствам
-            # TWILIGHT ZONE
-            # TODO: Debug errors
-            # temp = households.loc[households.index.intersection(dataset_infected_by_strain.sp_hh_id), ['latitude', 'longitude']]
-            #
-            # print("entered")
-            # #print(temp.index)
-            # #print(dataset_infected_by_strain.index)
-            # temp.index = dataset_infected_by_strain.index
-            # dataset_infected_by_strain['latitude'] = temp.latitude
-            # dataset_infected_by_strain['longitude'] = temp.longitude
-            # #print("passed")
-            #
-            #
-
-            # dataset_infected_by_strain[['sp_id']].to_csv(
-            #    r'results/infected_{}_seed_{}_day_{}.txt'.format(number_seed, j, key), sep='\t', index=False)
-
-            # Twilight zone border
             c_inf = int(data_current[data_current.infected ==
                         infIndexForStrain(key)]['infected'].sum())
 
@@ -477,42 +474,15 @@ def main_function(number_seed, dataset, dict_school_id_all, lmbd, days, dict_sch
                   int(data_current[['susceptible_'+key]].sum()), datetime.datetime.now()))
         print()
         df_results = pd.DataFrame.from_dict(results_dic)
-        df_results.to_csv(res_dir +
-                          'temp_results_{}.csv'.format(number_seed), sep='\t', index=False)
+        df_results.to_csv(
+            '../results/' + data_folder + r'/prevalence_seed_{}.csv'.format(number_seed), sep='\t', index=False)
 
-        pd.DataFrame(new_results_dic['H1N1']).to_csv(res_dir +
-                                                     r'new_infected_{}_seed_{}.txt'.format(number_seed, key), sep='\t', index=False)
+        # pd.DataFrame(new_results_dic['H1N1']).to_csv(
+        #     r'results/new_infected_{}_seed_{}.txt'.format(number_seed, key), sep='\t', index=False)
 
-        # Switching to compartmental submodel
-        if (switchToPopModelClause(j, mode="One_traj")):
-            # Prepare init data
-            # Берётся из функции Б-Р
-            q = [0.0, 0.0, 0.9, 0.9, 0.55, 0.3, 0.15, 0.05]
-
-            # strains = ["A(H1N1)pdm09", "A(H3N2)", "B"]
-
-            Phi_list = calcPhiForPop(new_results_dic)  # Incidence!
-
-            Immune_list = calcImmuneFractionForPop(data_current)
-            print("Immune list: ", Immune_list)
-            # Считаем, что переболевшие больше не заболеют вообще никаким гриппом
-            Recovered_num = calcRecoveredNumForPop(data_current)
-            pop_num = data_current.shape[0]
-            N = days[-1] - j
-            print(N)
-
-            y_model = model_test.launchFromABM(
-                Phi_list, Recovered_num, Immune_list, q, lmbd, pop_num, PERCENT_PROTECTED, j+1, N)
-
-            print(y_model)
-
-            for idx, key in enumerate(strains_keys):
-                # Нулевое значение пустое, не берём
-                results_dic[key].extend(list(y_model[idx])[1:])
-
-            print(results_dic)
-
-            return results_dic
+        df_incidence = pd.DataFrame(new_results_dic['H1N1'])
+        df_incidence.to_csv(
+            '../results/' + data_folder + r'/incidence_seed_{}.csv'.format(number_seed), sep='\t', index=False)
 
         data_current.loc[data_current.infected >
                          0, 'illness_day'] += 1  # Time goes
@@ -555,92 +525,61 @@ def main_function(number_seed, dataset, dict_school_id_all, lmbd, days, dict_sch
     return results_dic, number_seed, lmbd
 
 
-def gathering_function(sample_size_val, lmbd_val, alpha_val):
-    global sample_size
-    sample_size = sample_size_val
-
-    global lmbd
-    lmbd = lmbd_val
-
-    global alpha
-    alpha = alpha_val
-
-    data = pd.read_csv(r'./data/sampled/people_SAMPLED_{}.txt'.format(sample_size),
-                       sep='\t', index_col=0)
-    data = data[['sp_id', 'sp_hh_id', 'age', 'sex', 'work_id']]
-    households = pd.read_csv(
-        r'./data/sampled/households_SAMPLED_{}.txt'.format(sample_size), sep='\t')
-    households = households[['sp_id', 'latitude', 'longitude']]
-    dict_school_id_global = json.load(
-        open(os.path.expanduser(r'./data/sampled/schools_SAMPLED_{}.json'.format(sample_size))))
-
-    dict_school_len = [len(dict_school_id_global[i])
-                       for i in dict_school_id_global.keys()]
-
-    alpha_dic = {'H1N1': alpha, 'H3N2': 0.74, 'B': 0.6}
-
-    global MAGIC_PREHISTORY_LENGTH
-    MAGIC_PREHISTORY_LENGTH = 10
-
-    lmbd = 0.2
-    global num_runs
-    num_runs = 10
-    days = range(1, 100)
-
-    global PERCENT_PROTECTED
-    PERCENT_PROTECTED = 1  # Эмуляция разреженной сети, согласно RJNAMM
-
-    res_dir = "results/" + str(sample_size) + "_lmbd=" + \
-        str(lmbd) + "_alpha=" + str(alpha_dic['H1N1']) + "/"
-
-    start_all = time.perf_counter()
-
-    try:
-        os.makedirs("results/" + str(sample_size) + "_lmbd=" +
-                    str(lmbd) + "_alpha=" + str(alpha_dic['H1N1']) + "/")
-    except FileExistsError:
-        pass
-
-    data[['sp_id', 'sp_hh_id', 'age']] = data[[
-        'sp_id', 'sp_hh_id', 'age']].astype(int)
-    data[['work_id']] = data[['work_id']].astype(str)
-    # data = data.sample(frac=1)  # frac=1
-    households[['sp_id']] = households[['sp_id']].astype(int)
-    households[['latitude', 'longitude']] = households[[
-        'latitude', 'longitude']].astype(float)
-    households.index = households.sp_id
-
+def set_initial_values(data, strains_keys, alpha_dic):
     for key in strains_keys:  # По умолчанию все иммунные
         data['susceptible_'+key] = 0
 
     data['infected'] = 0
     data['illness_day'] = 0
 
-    cpu_num = mp.cpu_count()
-
-    print("{} processors detected".format(cpu_num))
-
     for key in strains_keys:
         data.loc[np.random.choice(data.index, round(len(
             data) * alpha_dic[key]), replace=False), 'susceptible_'+key] = 1  # Восприимчивые, доля альфа
+    return data
 
+
+if __name__ == '__main__':
+
+    start_all = time.perf_counter()
+
+    infected_init_dic = {'H1N1': 10, 'H3N2': 0, 'B': 0}
+    alpha_dic = {'H1N1': 0.78, 'H3N2': 0.74, 'B': 0.6}
+    lmbd = 0.15
+    num_runs = 10
+    days = range(1, 150)
+    strains_keys = ['H1N1', 'H3N2', 'B']
+
+    # in data folder schould be 3 files: 'people.txt', 'households.txt', 'schools.json'
+    data_folder = 'initial_samara/'
+    data_path = '../data/' + data_folder
+
+    results_dir = '../results/' + data_folder
+
+    if not os.path.exists(results_dir):
+        os.makedirs(results_dir)
+        print("Directory created successfully!")
+    else:
+        print("Directory already exists!")
+
+    data, households, dict_school_id = load_data(data_path)
+    data, households, dict_school_id = preprocess_data(
+        data, households, dict_school_id)
+    dict_school_len = [len(dict_school_id[i])
+                       for i in dict_school_id.keys()]
+    data = set_initial_values(data, strains_keys, alpha_dic)
+
+    print(data.head())
+
+    cpu_num = mp.cpu_count()
+
+    print("{} processors detected".format(cpu_num))
     output = {}
-    with mp.Pool(5) as pool:
-        output = pool.map(partial(main_function, dataset=data,
-                          dict_school_id_all=dict_school_id_global, lmbd=lmbd, days=days,
-                          dict_school_len=dict_school_len, res_dir=res_dir), range(5))
+    with mp.Pool(2) as pool:
+        output = pool.map(partial(main_function, data_folder=data_folder, dataset=data,
+                          dict_school_id_all=dict_school_id, lmbd=lmbd, infected_init_dic=infected_init_dic,
+                          days=days, dict_school_len=dict_school_len), range(2))
 
     finish_all = (time.perf_counter() - start_all)
     for i in output:
         print(i[1:])
     print("Total calculation time: {}".format(finish_all))
-
-
-if __name__ == '__main__':
-    sample_size_arr = [1000, 1500, 2000, 2500, 3000, 3500, 4000]
-    lmbd_arr = [0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6]
-    alpha_arr = [0.5, 0.6, 0.7, 0.8]
-    for sample_size in sample_size_arr:
-        for lmbd in lmbd_arr:
-            for alpha in alpha_arr:
-                gathering_function(sample_size, lmbd, alpha)
